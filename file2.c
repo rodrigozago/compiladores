afe22f9c7fad8af8488844eaba60c3b35bfe1c37
Rodrigo Zago <rodrigozago@me.com>
2020-12-10 14:09:55 -0300

    A

---
+++ file2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+typedef char literal[256];
+void main(void)
+{
+	/*----Variaveis temporarias----*/
+	int T0;
+	int T1;
+	int T2;
+	int T3;
+	int T4;
+	/*------------------------------*/
